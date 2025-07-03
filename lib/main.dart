e3a0e19eef0b86b857536291a1f0cdb098be385e
amran1451 <aeamarda@gmail.com>
2025-07-03 15:07:41 +0300

    исправление ошибок при компиляции

--- lib/main.dart
+++ lib/main.dart
@@ -18,27 +18,29 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  return MultiProvider(
-  providers: [
-  ChangeNotifierProvider(create: (_) => TaskProvider()),
-  ChangeNotifierProvider(create: (_) => ThemeProvider()),
-  ],
-  child: Consumer<ThemeProvider>(
-  builder: (context, themeProv, _) => MaterialApp(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Planner',
-        theme: ThemeData(primarySwatch: Colors.blue),
-  darkTheme: ThemeData.dark(),
-  themeMode: themeProv.themeMode,
-        initialRoute: '/',
-        routes: {
-          '/': (_) => const HomeScreen(),
-          '/edit': (_) => const TaskEditScreen(),
-          '/kanban': (_) => const KanbanScreen(),
-          '/dashboard': (_) => const DashboardScreen(),
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProv, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Planner',
+            theme: ThemeData(primarySwatch: Colors.blue),
+            darkTheme: ThemeData.dark(),
+            themeMode: themeProv.themeMode,
+            initialRoute: '/',
+            routes: {
+              '/': (_) => const HomeScreen(),
+              '/edit': (_) => const TaskEditScreen(),
+              '/kanban': (_) => const KanbanScreen(),
+              '/dashboard': (_) => const DashboardScreen(),
+            },
+          );
         },
-  ),
       ),
     );
   }
